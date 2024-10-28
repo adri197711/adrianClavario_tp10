1b02438d4388f368607f3aba06e68de9a86779fb
Adrián Clavario <adrianclavario@gmail.com>
2024-10-28 20:50:00 -0300

    corrijo variables

--- operaciones.js
+++ operaciones.js
@@ -1,11 +1,17 @@
-const numero1 = parseInt(20);
-const numero2 = parseInt(23);
+let numero1 = parseInt(20);
+let numero2 = parseInt(23);
 
-console.log('suma: ' + (numero1 + numero2));
-console.log('resta: ' + (numero1 - numero2));
-console.log('multiplicación: ' + numero1 * numero2);
-console.log('división: ' + numero1 / numero2);
-console.log('módulo: ' + numero1 % numero2);
+const suma = numero1 + numero2;
+const resta = numero1 - numero2;
+const multiplicacion = numero1 * numero2;
+const division = numero1 / numero2;
+const modulo = numero1 % numero2;
+
+console.log('suma: ' + suma);
+console.log('resta: ' + resta);
+console.log('multiplicación: ' + multiplicacion);
+console.log('división: ' + division);
+console.log('módulo: ' + modulo);
 
 function mayor(numero1, numero2){
   if(numero1 > numero2){
