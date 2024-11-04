fc2544e4059411b8e95c2220059f05099faf4f7d
Adrián Clavario <adrianclavario@gmail.com>
2024-11-04 20:44:57 -0300

    subo archivo boletosCorregido.js, con los cambios correspondientes

---
+++ boletosCorregido.js
@@ -0,0 +1,20 @@
+
+let nombre = "Adrian";
+let apellido = "Clavario";
+let edad = 47;
+let telefono = 1531450597;
+let socio = true;
+let fechaPartido = new Date(2024, 12, 3, 18);
+let numeroSilla = 11;
+let precioBoleto = 18.75;
+
+console.log(nombre, "Tipo de dato: " + typeof (nombre));
+console.log(apellido, "Tipo de dato: " + typeof (apellido));
+console.log(edad, "Tipo de dato: " + typeof (edad));
+console.log(telefono, "Tipo de dato: " + typeof (telefono));
+console.log(socio, "Tipo de dato: " + typeof (socio));
+console.log(fechaPartido, "Tipo de dato: " + typeof (fechaPartido));
+console.log(numeroSilla, "Tipo de dato: " + typeof (numeroSilla));
+console.log(precioBoleto, "Tipo de dato: " + typeof (precioBoleto));
+
+
